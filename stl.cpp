91eb7c8e4ba1e34291491d8174645b1014b10ccc
ankitkushwaha4123 <ankitkushwahaak4123@gmail.com>
2025-08-30 17:39:16 +0530

    stl code

---
+++ stl.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print()
+
+{
+
+    cout<<"ankit";
+}
+int sum(int a,int b)
+{
+    return a+b;
+}
+int main()
+
+{
+    
+    print();
+    int s=sum(10,20);
+    cout<<s;
+    return 0;
+}
